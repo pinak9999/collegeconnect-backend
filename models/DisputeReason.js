80ac6f3244f8edabb15816c505975441ce2871de
Pinak <davepinak0@gmail.com>
2026-02-18 19:02:57 +0530

    Created missing StudentBookings page

--- models/DisputeReason.js
+++ models/DisputeReason.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const DisputeReasonSchema = new mongoose.Schema({
-  reason: {
-    type: String,
-    required: true
-  }
+const DisputeReasonSchema = new Schema({
+    reason: {
+        type: String,
+        required: true,
+        unique: true
+    }
 });
 
-module.exports = mongoose.model('DisputeReason', DisputeReasonSchema);
+module.exports = DisputeReason = mongoose.model('disputereason', DisputeReasonSchema);
