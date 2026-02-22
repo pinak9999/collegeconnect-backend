d966c3803c824fc54697b0da94b386f0258b1889
Pinak <davepinak0@gmail.com>
2026-02-22 18:16:54 +0530

    Created missing StudentBookings page

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const cors = require('cors');
 const http = require('http'); 
 const { Server } = require("socket.io"); 
 const Message = require('./models/Message'); 
-// 1. सबसे ऊपर Import करो
 
-// 2. जहाँ तुम्हारे सारे <Route> हैं, वहाँ ये लाइन जोड़ दो:
-<Route path="/about" element={<About />} />
 // 1. App aur Server setup
 const app = express();
 const server = http.createServer(app);
